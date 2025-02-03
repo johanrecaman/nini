5fcd5244da1f6891da2491cda725d9a4a22f8bfe
Johan Recaman <johanstrr@gmail.com>
2025-02-03 20:04:06 -0300

    day 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 20, 2025 20:00:00").getTime();
+var countDownDate = new Date("Feb 20, 2025 19:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
